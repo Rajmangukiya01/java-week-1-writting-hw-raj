077fb12d3c043cac9e8bd573dbe58b8423ff495c
Rajmangukiya01 <rajmangukiya1002gmail.com>
2022-10-15 00:26:30 +0100

    My java week 1 writting hw raj

---
+++ src/homework_week_5/Programme_1_BioData.java
@@ -0,0 +1,22 @@
+package homework_week_5;
+/**
+ * 1. Write a program that prints your BIO Data.
+ */
+public class Programme_1_BioData {
+    public static void main(String[] args) {
+        System.out.println("              BIO DATA                ");
+        System.out.println("Name              : Raj Mangukiya");
+        System.out.println("Mobile            : 07717 003663");
+        System.out.println("Email             : rajmangukiya100@gmail.com");
+        System.out.println("Gender            : Male");
+        System.out.println("Date of Birth     : 01.05.1999");
+        System.out.println("Marital Status    : Single");
+        System.out.println("Languages Known   : English, Gujarati and Hindi");
+        System.out.println("Qualification     : Software Tester");
+        System.out.println("Experience        : Agile Tester with 5 years of experience");
+        System.out.println("Address           : 123 Abc Street, London AB1 2CD");
+        System.out.println("Place             : London");
+        System.out.println("Date              : 13.10.2021");
+        System.out.println("Signature         : Raj");
+    }
+}
